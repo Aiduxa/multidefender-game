ae211cccf6acfa88622efafe55b1b9d8c34b7019
IgnasStaskevicius <ignas.staskevicius@stud.ktmc.lt>
2023-11-16 13:12:01 +0200

    'towerV1.1'

--- src/objects/tower.js
+++ src/objects/tower.js
@@ -1,8 +1,9 @@
 class Tower {
-    constructor(type, name, health, damage) {
+    constructor(type, name, health, damage, owner) {
         this.type = type;
         this.name = name;
         this.health = health;
         this.damage = damage;
+        this.owner = owner;
     }
 }
