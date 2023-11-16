899652a6d80d598488b31ef19228625fa0c3847f
aidas <aidas6900gt@gmail.com>
2023-11-16 12:58:12 +0200

    Simple player.py

---
+++ src/objects/player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor(id, name, level, money, defences) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.money = money;
+        this.defences = defences;
+    }
+}
