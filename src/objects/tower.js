cb2a65277aabaf21d13e120c572398798a84b050
IgnasStaskevicius <ignas.staskevicius@stud.ktmc.lt>
2023-11-16 13:09:48 +0200

    'tower'

---
+++ src/objects/tower.js
@@ -0,0 +1,8 @@
+class Tower {
+    constructor(type, name, health, damage) {
+        this.type = type;
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+}
