76881248c667db656638257c410609e1a5c5fccd
aidas <aidas6900gt@gmail.com>
2023-11-16 17:52:16 +0200

    Api

--- api/main.js
+++ api/main.js
@@ -3,8 +3,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-var mysql = require("mysql");
-var jwt = require("jwt");
+var jwt = require("jsonwebtoken");
+var hash = require("jshashes")
 
 
 const app = express();
@@ -14,19 +14,61 @@ app.use(bodyParser.raw());
 
 const port = 3000;
 
-var con = mysql.createConnection({
-    host: "",
-    user: "",
-    password: "",
-    database: "",
-});
+async function getConnection() {
+    var mysql = require("mysql2/promise");
+    return con = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "multidefender",
+    });
+}
 
-con.connect(function (err) {
-    if (err) throw err;
-});
 
 app.get("/", (req, res) => {
     res.send("Labas pasauli!");
+});
+
+app.post("/api/register", async (req, res) => {
+    var SHA256 = new hash.SHA256;
+    let name = req.body.name;
+    let password = SHA256.hex(req.body.password);
+    let id;
+
+    con = await getConnection();
+
+    console.log(3)
+    await con.query("SELECT * FROM users WHERE name = ?", [name], function(err, result, fields) {
+        console.log(2);
+        if (err) throw err;
+        console.log(result);
+        if (result.length()) {
+            console.log(1);
+            return res.json({"error": "userexists"});
+        };
+    });
+
+
+    // let sql = "INSERT INTO users(name, password) VALUES (?, ?)";
+    // await con.query(sql, [name, password, name], function (err, result, fields) {
+    //     if (err) throw err;
+    // });
+    
+    id = await con.query("SELECT id FROM users WHERE name = ?", [name], function (err, result, fields) {
+        if (err) throw err;
+        
+        return result
+
+    });
+    
+
+    let token = jwt.sign({'id': id[0][0].id, 'name': name, 'password': password, 'random-point': Math.random()}, "secret-key-multidefendergame");
+    con.query("INSERT INTO session(token, user_id) VALUES (?, ?)", [token, id[0][0].id], function(err, result, fields) {
+        if (err) throw err;
+
+    })
+
+    return res.json({"token": token, "id": id[0][0].id});
 });
 
 app.get("/api/login", (req, res) => {
@@ -37,14 +79,12 @@ app.get("/api/login", (req, res) => {
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
         
-        
-    });
-
-    
-
-    res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, { "Content-Type": "application/json" });
         res.write(JSON.stringify(result));
         return res.end();
+    });
+
+    let token = jwt.sign(data={})
 });
 
 // Authentification
