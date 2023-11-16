93a5d39d46efa575f0013223ebe0ab8dc5e7a087
aidas <aidas6900gt@gmail.com>
2023-11-16 12:50:06 +0200

    Enemy object

---
+++ src/objects/enemy.js
@@ -0,0 +1,14 @@
+class Enemy{
+    constructor(type, name, health, attack, speed, sent_by) {
+        this.type = type;
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.speed = speed;
+        this.sent_by = sent_by;
+    }
+    
+    function () {
+        // Nepamenu kaip ce toleliau darit
+    }
+}
